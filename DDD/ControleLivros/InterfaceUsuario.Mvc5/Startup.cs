a632311ec6ff83757a278285e3adefd3d29978b2
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-09-26 16:38:51 -0300

    Camada de UI
    Camada que faz a interação entre o usuário e o domínio desenvolvido.
    Essa camada é desenvli

---
+++ DDD/ControleLivros/InterfaceUsuario.Mvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterfaceUsuario.Mvc5.Startup))]
+namespace InterfaceUsuario.Mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
