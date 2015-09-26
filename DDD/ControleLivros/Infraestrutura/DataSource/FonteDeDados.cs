99bd5e3827069ec70044a57b8a1a6e34ff2ca0cf
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-09-26 16:25:38 -0300

    Camada de Infraestrutura
    Camada responsável pela parte de instrumentação e automação do trabalho:
    Frameworks, Containers de injeção de dependências e outros artefatos que
    ficam transparanetes para o cliente.

---
+++ DDD/ControleLivros/Infraestrutura/DataSource/FonteDeDados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.DataSource
+{
+    public class FonteDeDados: DbContext
+    {
+        #region Atributos
+        #endregion
+        #region Construtores
+        public FonteDeDados()
+            :base(ConfigurationManager.ConnectionStrings["Exercicio01"].ConnectionString)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ DDD/ControleLivros/Infraestrutura/Repositorios/AutorRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Framework.Arquitetura.DDD.InfraEstrutura.Repositorios.EF;
+using Framework.Arquitetura.DDD.Dominio.Interfaces.Repositorios;
+using Dominio.Entidades;
+using Dominio.Interfaces.Repositorios;
+using Infraestrutura.DataSource;
+
+namespace Infraestrutura.Repositorios
+{
+    public class AutorRepositorio : RepositorioBase<Autor, FonteDeDados>, IAutorRepositorio
+    {
+
+    }
+}
