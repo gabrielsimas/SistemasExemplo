81996d183baeed96c7286d96bf84770360852174
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-09-28 07:18:41 -0300

    Acerto de repositório
    Acerto de repositório de Tipo de Rede Social.

--- DDD/ControleContatos/Dominio/Interfaces/Repositorios/ITipoRedeSocialRepositorio.cs
+++ DDD/ControleContatos/Dominio/Interfaces/Repositorios/ITipoRedeSocialRepositorio.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio.Entidades;
+using Framework.Arquitetura.DDD.Dominio.Interfaces.Repositorios;
 
 namespace Dominio.Interfaces.Repositorios
 {
-    public interface ITipoRedeSocialRepositorio<TipoRedeSocial>
+    public interface ITipoRedeSocialRepositorio: IRepositorioBase<TipoRedeSocial>
     {
     }
 }
