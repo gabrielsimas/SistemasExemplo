9c0ecf0dc3af8640e91fa526dab5cd086d52824c
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-09-28 06:28:09 -0300

    Camada de Dominio - Interface de Serviços

---
+++ DDD/ControleContatos/Dominio/Interfaces/Servicos/ITipoTelefoneServico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Entidades;
+using Framework.Arquitetura.DDD.Dominio.Interfaces.ServicosDeDominio;
+
+namespace Dominio.Interfaces.Servicos
+{
+    public interface ITipoTelefoneServico: IServicoDeDominioBase<TipoTelefone>
+    {
+    }
+}
