dcba3de1c5869e505f2cbb94d3e250ba8f06c46b
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-10-03 00:26:19 -0300

    Implementação do Repositorio e UnitOfWork
    Implementação do Repositório.

---
+++ DDD/ControleLivros_Unity/ArtigoUnity.Infraestrutura/EF/Repositorio/Implementacao/LivroRepositorio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtigoUnity.Dominio.Entidade;
+using ArtigoUnity.Dominio.Repositorio.Interfaces;
+
+namespace ArtigoUnity.Infraestrutura.EF.Repositorio.Implementacao
+{
+    public class LivroRepositorio: UnityOfWork,  ILivroRepositorio
+    {
+        #region Atributos
+
+        #endregion
+
+        #region Construtores
+
+        #endregion
+
+        #region Métodos do Repositório
+
+        #endregion
+        public void Alterar(Livro entidade)
+        {
+            
+        }
+
+        public Livro BuscarPorId(long? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Livro> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Livro entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Livro FiltrarUmPor(Func<Livro, bool> predicado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Livro> FiltrarVariosPor(Func<Livro, bool> predicado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar(Livro entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DDD/ControleLivros_Unity/ArtigoUnity.Infraestrutura/EF/Repositorio/Implementacao/UnityOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtigoUnity.Dominio.Repositorio.Interfaces.Base;
+
+namespace ArtigoUnity.Infraestrutura.EF.Repositorio.Implementacao
+{
+    public class UnityOfWork: IUnitOfWork
+    {
+        public void CommitAlteracoes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommitaERefresha()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesfazAlteracoes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
