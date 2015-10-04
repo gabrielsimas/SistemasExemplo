88946f34e6bb95a624e7b4d6109f1b7d21024295
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2015-10-04 02:28:13 -0300

    Camada de Infra estrutura - IoC
    Criamos o objeto do Unity para Injeção de dependências.

---
+++ DDD/ControleLivros_Unity/ArtigoUnity.Infraestrutura/IoC/MicrosoftUnity/ContainerDoUnity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtigoUnity.Dominio.Repositorio.Interfaces;
+using ArtigoUnity.Infraestrutura.EF.ContextoBD;
+using ArtigoUnity.Infraestrutura.EF.Repositorio.Implementacao;
+using Microsoft.Practices.Unity;
+
+namespace ArtigoUnity.Infraestrutura.IoC.MicrosoftUnity
+{
+    public static class ContainerDoUnity
+    {
+
+        #region Atributos
+        static IUnityContainer container;
+        #endregion
+
+        #region Construtores
+        static ContainerDoUnity()
+        {
+            CriaContainer();
+        }
+        #endregion
+
+        #region Propriedades
+        public static IUnityContainer Container
+        {
+            get
+            {
+                return container;
+            }
+        }
+        #endregion
+
+        #region Injeção de Dependências
+        static void CriaContainer()
+        {
+            container = new UnityContainer();
+
+            //Repositorios
+            container.RegisterType<DbContext, ContextoDb>(new HierarchicalLifetimeManager());
+            container.RegisterType<IEditoraRepositorio, EditoraRepositorio>(new InjectionConstructor(container.Resolve<ContextoDb>()));
+            container.RegisterType<ILivroRepositorio, LivroRepositorio>(new InjectionConstructor(container.Resolve<ContextoDb>()));
+        }
+
+        public static IUnityContainer GetContainer()
+        {            
+            CriaContainer();
+            return container;           
+        }
+
+        #endregion
+
+    }
+}
