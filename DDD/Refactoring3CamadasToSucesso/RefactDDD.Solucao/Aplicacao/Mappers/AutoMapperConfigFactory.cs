7a77616224c798a3eb30be285e23eb60477a30d7
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2016-02-14 12:13:57 -0200

    Camada de Aplicação
    - Refactoring da forma de como fazer a transcrição entre Dto e Domínio.
    Substituímos a classe Montador pelo framework AutoMapper por sua
    facilidade.

---
+++ DDD/Refactoring3CamadasToSucesso/RefactDDD.Solucao/Aplicacao/Mappers/AutoMapperConfigFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplicacao.Dto;
+using AutoMapper;
+using Dominio.Entidade;
+
+namespace Aplicacao.Mappers
+{
+    public class AutoMapperConfigFactory
+    {
+        private AutoMapperConfigFactory()
+        {
+
+        }
+
+        private static IMapper RegisterProfiles()
+        {
+            var config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<Tarefa, TarefaDto>()                        
+                        .ForMember(dto => dto.Usuario, opt => opt.Ignore())
+                        .ReverseMap();
+                        
+
+                    //cfg.CreateMap<TarefaDto, Tarefa>()
+                    //    .ForMember(entd => entd.Usuario, dto => dto.MapFrom(d => d.Usuario));                        
+
+                    cfg.CreateMap<Usuario, UsuarioDto>()
+                        .ForMember(dto => dto.Tarefas, entd => entd.MapFrom(e => e.Tarefas))
+                        .ReverseMap();
+                }
+            );
+
+            var mapper = config.CreateMapper();
+
+            return mapper;
+
+        }
+
+        public static IMapper GetMapper()
+        {
+            return RegisterProfiles();
+        }
+    }
+}
