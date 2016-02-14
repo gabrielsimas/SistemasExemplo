bd4598db16ae6eff545b2981a18cf84be5bda202
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2016-02-14 12:08:08 -0200

    LivroController
    Controlador dos Livros na Camada de Apresentação

--- DDD/ControleLivros_Unity/ArtigoUnity.Web.Mvc/Controllers/LivroController.cs
+++ DDD/ControleLivros_Unity/ArtigoUnity.Web.Mvc/Controllers/LivroController.cs
@@ -17,78 +17,78 @@ namespace ArtigoUnity.Web.Mvc.Controllers
 		private ILivroAplicServico livroServico;
 		private IEditoraAplicServico editoraServico;
 
-        public LivroController(ILivroAplicServico livroServico, IEditoraAplicServico editoraServico)
-        {
-            this.livroServico = livroServico;
-            this.editoraServico = editoraServico;
-        }
+		public LivroController(ILivroAplicServico livroServico, IEditoraAplicServico editoraServico)
+		{
+			this.livroServico = livroServico;
+			this.editoraServico = editoraServico;
+		}
 
-        public ActionResult Listar()
-        {
-            ICollection<LivroModelConsulta> dtos = ListarLivros();
-            return View("Listar", dtos);
-        }
+		public ActionResult Listar()
+		{
+			ICollection<LivroModelConsulta> dtos = ListarLivros();
+			return View("Listar", dtos);
+		}
 
-        public ICollection<LivroModelConsulta> ListarLivros()
-        {
-            ICollection<LivroDto> dtos = livroServico.ListarTodosOsLivros();
+		public ICollection<LivroModelConsulta> ListarLivros()
+		{
+			ICollection<LivroDto> dtos = livroServico.ListarTodosOsLivros();
 
-            if (dtos != null && dtos.Count > 0)
-            {
+			if (dtos != null && dtos.Count > 0)
+			{
 
-                ICollection<LivroModelConsulta> livros = new List<LivroModelConsulta>();
+				ICollection<LivroModelConsulta> livros = new List<LivroModelConsulta>();
 
-                foreach (LivroDto linha in dtos)
-                {
-                    LivroModelConsulta livro = new LivroModelConsulta()
-                    {
-                        Id = linha.Id,
-                        Autor = linha.Autor,
-                        Genero = linha.Genero,
-                        Isbn = linha.Isbn,
-                        Sinopse = linha.Sinopse,
-                        Titulo = linha.Titulo
-                    };
+				foreach (LivroDto linha in dtos)
+				{
+					LivroModelConsulta livro = new LivroModelConsulta()
+					{
+						Id = linha.Id,
+						Autor = linha.Autor,
+						Genero = linha.Genero,
+						Isbn = linha.Isbn,
+						Sinopse = linha.Sinopse,
+						Titulo = linha.Titulo
+					};
 
-                    livros.Add(livro);
-                }
+					livros.Add(livro);
+				}
 
-                return livros;
-            }
+				return livros;
+			}
 
-            return null;
+			return null;
 
-        }
+		}
 
 		public ActionResult Cadastrar()
 		{
-            LivroModelCadastro cadastro = new LivroModelCadastro();
-            return View(cadastro);
+			LivroModelCadastro cadastro = new LivroModelCadastro();
+			return View(cadastro);
 		}		
 
 		[HttpPost]
 		public ActionResult CadastrarLivro(LivroModelCadastro model)
 		{
-            try
-            {
-                LivroDto livro = new LivroDto()
-                {
-                    Autor = model.Autor,
-                    Genero = model.Genero,
-                    Isbn = model.Isbn,
-                    Sinopse = model.Sinopse,
-                    Titulo = model.Titulo
-                };
+			try
+			{
+				LivroDto livro = new LivroDto()
+				{
+					Autor = model.Autor,
+					Genero = model.Genero,
+					Isbn = model.Isbn,
+					Sinopse = model.Sinopse,
+					Titulo = model.Titulo
+				};
 
-                livroServico.CadastrarLivro(livro, null);
+				livroServico.CadastrarLivro(livro, null);
 
-                model = new LivroModelCadastro();
+				model = new LivroModelCadastro();
 
-            } catch(Exception ex){
-                ViewBag.Mensagem = String.Format("Erro salvando Livro {0}: {1}",model.Titulo, ex.Message);
-            }
+			} catch(Exception ex){
+				ViewBag.Mensagem = String.Format("Erro salvando Livro {0}: {1}",model.Titulo, ex.Message);
+			}
 
-            return View("Cadastrar",model);
+			return View("Cadastrar",model);
 		}		
 
 		public ActionResult FiltrarLivros(LivroModelFiltro model) 
