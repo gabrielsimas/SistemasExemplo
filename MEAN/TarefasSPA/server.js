efe6a44cc354e3d8047c64d3f2234206c3414664
Luís Gabriel Nascimento Simas <gabrielsimas@gmail.com>
2016-04-20 21:25:59 -0300

    Configuração do Servidor e Web Api do Node.Js
    Arquivo contendo as configurações do Node com Express e Mongoose
    contendo as configurações para subida do servidor e os serviços do
    mesmo.

---
+++ MEAN/TarefasSPA/server.js
@@ -0,0 +1,94 @@
+
+var express = require('express');
+
+//Cria a nossa aplicação utilizando o Express
+var aplicacao = express();
+
+//Mongoose para o MongoDB
+var mongoose = require('mongoose');
+
+//Envio de logs para o console com Morgan
+var morgan = require('morgan');
+
+//Envia informações via HTML POST
+var bodyParser = require('body-parser');
+
+//Simula os métodos HTTP PUT e DELETE
+var methodOverride = require('method-override');
+
+//Configuração
+
+//Conectando-se ao Mongo via Mongoose no modulus.io
+// connect to mongoDB database on modulus.io
+
+//mongoose.connect('mongodb://node:nodeuser@localhost/senha');
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+
+// set the static files location /public/img will be /img for users
+// Configure o local para os arquivos estáticos /public/img para /img para os usuários e urls
+aplicacao.use(express.static(__dirname + '/Publico'));
+
+//Manda tudo pro console
+aplicacao.use(morgan('dev'));
+
+//faz um parse através da codificação application/x-www-form-urlencoded
+aplicacao.use(bodyParser.urlencoded({'extended':'true'}));
+
+//faz um parse de application/json
+aplicacao.use(bodyParser.json());
+
+//faz um parse de application/vnd.api+json para json 
+aplicacao.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+aplicacao.use(methodOverride());
+
+//Modelos da aplicação através do Mongoose
+var Tarefa = mongoose.model('Tarefa',{
+	texto: String
+	});
+
+
+//Rotas ===============================================================================================
+
+//API
+
+//GET todas as tarefas
+aplicacao.get('/api/tarefas',pegaTodasAsTarefas);
+
+// Cria uma tarefa e como resposta devolve todas as tarefas criadas
+aplicacao.post('/api/tarefas,')
+
+function pegaTodasAsTarefas(requisicao,resposta){	
+
+	Tarefa.find(findAll);	
+
+	function findAll(erro,tarefas){
+	if (erro){
+			resposta.send(erro);
+		}	
+		
+	resposta.json(tarefas);
+	}
+}
+
+function criaTarefa(requisicao,resposta){
+
+	var tarefaModel = {
+		texto: requisicao.body.texto,
+		executada: false
+	};
+
+	Tarefa.create(tarefaModel,create);
+
+	function create(erro,tarefa){
+		if (erro) resposta.send(erro)		
+	}
+
+	pegaTodasAsTarefas;
+	
+}
+
+
+//Inicio do servidor com a aplicação
+aplicacao.listen(9900);
+console.log("Gerenciado de Tarefas rodando na porta 9900");
